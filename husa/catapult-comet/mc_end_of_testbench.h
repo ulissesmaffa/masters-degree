6b505fe01d1d8a3b7dde206d0b0eb37af4c93234
Ulisses Maffazioli <ulissesmaffa@gmail.com>
2024-08-06 14:54:58 -0300

    ajust

--- husa/catapult-comet/mc_end_of_testbench.h
+++
@@ -1,9 +0,0 @@
-#ifndef MC_END_OF_TESTBENCH_H
-#define MC_END_OF_TESTBENCH_H
-
-struct mc_end_of_testbench { 
-  virtual void end_of_testbench() = 0; 
-  virtual ~mc_end_of_testbench() {};
-};
-
-#endif
